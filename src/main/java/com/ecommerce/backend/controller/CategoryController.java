4590c5d61b23d4b94d26ebe7f7c0518192f1eb6d
KhaiCX <khaicx2001@gmail.com>
2025-12-20 16:09:47 +0700

    Format Category controller

--- src/main/java/com/ecommerce/backend/controller/CategoryController.java
+++ src/main/java/com/ecommerce/backend/controller/CategoryController.java
@@ -21,37 +21,50 @@ public class CategoryController {
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin/categories")
     public ResponseEntity<List<CategoryResponse>> getDataCategories() {
-        return ResponseEntity.ok(categoryService.getCategories());
+        return ResponseEntity
+                .ok(categoryService.getCategories());
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin/categories/{id}")
     public ResponseEntity<CategoryResponse> getById(@PathVariable UUID id) {
-        return ResponseEntity.ok().body(categoryService.getById(id));
+        return ResponseEntity
+                .ok()
+                .body(categoryService.getById(id));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/admin/categories")
     public ResponseEntity<CategoryResponse> addCategory(@RequestBody CategoryRequest request) {
-        return ResponseEntity.ok(categoryService.addCategory(request));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(categoryService.addCategory(request));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/admin/categories/{id}")
-    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable UUID id, @RequestBody CategoryRequest request) {
-        return ResponseEntity.ok(categoryService.updateCategoryById(request, id));
+    public ResponseEntity<CategoryResponse> updateCategory(
+            @PathVariable UUID id,
+            @RequestBody CategoryRequest request) {
+        return ResponseEntity
+                .ok(categoryService.updateCategoryById(request, id));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/admin/categories/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable UUID id, @RequestBody SoftDeleteRequest request) {
+    public ResponseEntity<Void> deleteById(
+            @PathVariable UUID id,
+            @RequestBody SoftDeleteRequest request) {
         categoryService.deleteById(id, request);
-        return ResponseEntity.status(HttpStatus.OK.value()).build();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
     }
 
     @GetMapping("/categories")
     public ResponseEntity<List<CategoryResponse>> getAllForUser() {
-        return ResponseEntity.ok(categoryService.getAllForUser());
+        return ResponseEntity
+                .ok(categoryService.getAllForUser());
     }
 
 }
